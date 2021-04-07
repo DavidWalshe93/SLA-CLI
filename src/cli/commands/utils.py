9fb7a56bde81eaf7ff61cc386e4e59161fb00212
DavidWalshe93 <david.walshe93@gmail.com>
2021-04-07 16:50:18 +0100

    Added command wrapper to convert command options into a dataclass.

---
+++ src/cli/commands/utils.py
@@ -0,0 +1,27 @@
+"""
+Author:     David Walshe
+Date:       07 April 2021
+"""
+
+import logging
+
+logger = logging.getLogger(__name__)
+
+
+def kwargs_to_dataclass(data_class):
+    """
+    Converts a click commands option arguments into a python dataclass
+
+    :param data_class: A dataclass reference to create and inject as the command's param object.
+    """
+
+    def _kwargs_to_dataclass(func: callable):
+        @wraps(func)
+        def _kwargs_to_dataclass_wrapper(*args, **kwargs):
+            command_args = data_class(**kwargs)
+
+            return func(command_args)
+
+        return _kwargs_to_dataclass_wrapper
+
+    return _kwargs_to_dataclass
