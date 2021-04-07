2fe21a19241a7fb9478a6a4f74e1a609ec647e0c
DavidWalshe93 <david.walshe93@gmail.com>
2021-04-07 16:51:38 +0100

    Added boiler plate CLI command entry point.

--- cli.py
+++ cli.py
@@ -5,4 +5,39 @@ Date:       07 April 2021
 
 import logging
 
+import click
+
+from src.cli.config import GROUP_CONTEXT_SETTINGS
+
 logger = logging.getLogger(__name__)
+
+
+@click.group(**GROUP_CONTEXT_SETTINGS)
+def cli(**kwargs):
+    """
+    Base SL-CLI command.
+    """
+    pass
+
+
+# ==================================================
+# Add CLI groups
+# ==================================================
+groups = [
+
+]
+
+for group in groups:
+    cli.add_command(group)
+
+# ==================================================
+# Add CLI commands
+# ==================================================
+commands = [
+]
+
+for command in commands:
+    cli.add_command(command)
+
+if __name__ == '__main__':
+    cli()
