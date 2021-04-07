0ac15e1d172b69c005f1e8f38094f0de749b699a
DavidWalshe93 <david.walshe93@gmail.com>
2021-04-07 16:42:40 +0100

    Added context settings for Click

---
+++ src/cli/commands/context.py
@@ -0,0 +1,23 @@
+"""
+Author:     David Walshe
+Date:       07 April 2021
+"""
+
+import logging
+
+logger = logging.getLogger(__name__)
+
+
+# ===========================================================
+# Click Context Options
+# ===========================================================
+COMMAND_CONTEXT_SETTINGS = {
+    "context_settings": {
+        "help_option_names": ["-h", "--help"]
+    }
+}
+
+GROUP_CONTEXT_SETTINGS = {
+    **COMMAND_CONTEXT_SETTINGS,
+    "invoke_without_command": True
+}
