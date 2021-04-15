cd8436f58e6e8882f8effefcece8c7eb5ba015ab
DavidWalshe93 <david.walshe93@gmail.com>
2021-04-16 00:18:06 +0100

    Added filtering and fixed train/test splitting to organise command.

--- sla_cli/src/cli/commands/organise.py
+++ sla_cli/src/cli/commands/organise.py
@@ -6,7 +6,7 @@ Date:       14 April 2021
 import logging
 import logging
 import os
-from typing import List
+from typing import List, Union, Tuple
 from dataclasses import dataclass, asdict
 import hashlib
 
@@ -33,6 +33,32 @@ class OrganiseParameters:
     fixed_test_set: str
     fixed_train_set: str
     split: TrainTestSplit
+    stratify: bool
+
+
+def read_ids_cb(ctx, param, file_path: str) -> List[str]:
+    """
+    Reads a csv file and returns the content of the id row.
+
+    :param file_path: The file path to read the csv from.
+    :return: A list of IDs.
+    """
+    if file_path is None:
+        return None
+    try:
+        return list(pd.read_csv(file_path)["id"])
+    except Exception as err:
+        raise click.BadParameter(f"Cannot read 'ids' from file '{file_path}'.", ctx, param)
+
+
+def method_cb(ctx, param, method) -> str:
+    """Returns the type of method used for capture as seen in the metadata."""
+    return {
+        "biopsy": "histopathology",
+        "single": "single image expert consensus",
+        "serial": "serial imaging showing no change",
+        "confocal": "confocal microscopy with consensus dermoscopy",
+    }.get(method, "all")
 
 
 @click.command(**COMMAND_CONTEXT_SETTINGS, short_help="Organises datasets into train/validation/splits.")
@@ -43,15 +69,16 @@ class OrganiseParameters:
 @click.option("-e", "--exclude", type=click.STRING, multiple=True, default=None, callback=match_dx_cb,
               help="Used to include specific classes in the data. Option in mutually exclusive to '-i/--include'.")
 @click.option("-m", "--method", type=click.Choice(["biopsy", "confocal", "serial", "single", "all"], case_sensitive=False),
-              default="all", help="The method by which the lesion diagnosis was reached. Defaults to 'all'.")
-@click.option("--fixed-test-set", type=click.File(), default=None, help="Specifies a fixed test set provided as a a csv with an 'id' field.")
-@click.option("--fixed-train-set", type=click.File(), default=None, help="Specifies a fixed training set provided as a a csv with an 'id' field.")
+              default="all", callback=method_cb, help="The method by which the lesion diagnosis was reached. Defaults to 'all'.")
+@click.option("--fixed-test-set", type=click.STRING, default=None, callback=read_ids_cb, help="Specifies a fixed test set provided as a a csv with an 'id' field.")
+@click.option("--fixed-train-set", type=click.STRING, default=None, callback=read_ids_cb, help="Specifies a fixed training set provided as a a csv with an 'id' field.")
 @click.option("-s", "--split", type=click.STRING, default=".7:.15:.15", metavar="<TRAIN>:<VAL>:<TEST>", callback=train_test_split_cb,
               help="Specifies the dataset split ratio. Inputted as TRAIN:VAL:TEST. "
                    "\n\nFloating-point values will provide ratio splits, sum of which must = 1."
                    "\n\nInteger values provide the number of instances for each split."
                    "\n\nDefaults to train=70%, val=15%, test=15%."
                    "\n\nTRAIN and TEST values are ignored if --fixed-test-set/--fixed-train-set is specified.")
+@click.option("--stratify", is_flag=True, help="If using split with floating point numbers, this will enable stratification on the 'dx' field.")
 @kwargs_to_dataclass(OrganiseParameters)
 @click.pass_context
 def organise(ctx: Context, params: OrganiseParameters):
@@ -75,9 +102,17 @@ def organise(ctx: Context, params: OrganiseParameters):
     df = remove_excludes(params.exclude, df)
     df = keep_dx_type(params.method, df)
 
+    # train_df, val_df, test_df = None, None, None
+
+    logger.info(f"Total instances found after filtering: {df.shape[0]}")
+
+    df, test_df = extract_fixed_set(params.fixed_test_set, df, "test_set")
+    df, train_df = extract_fixed_set(params.fixed_train_set, df, "train_set")
+
+    print("test", test_df.shape[0])
+    print("train", train_df)
     print(df.shape[0])
 
-    print(params)
 
 def gather_metadata(data_directory: str, datasets: List[str], available_datasets: List[str]) -> List[pd.DataFrame]:
     """
@@ -136,3 +171,21 @@ def keep_dx_type(dx_type: str, metadata: pd.DataFrame) -> pd.DataFrame:
         return metadata
 
     return metadata[metadata["dx_type"] == dx_type]
+
+
+def extract_fixed_set(fixed_set: List[str], df: pd.DataFrame, set_type: str) -> Tuple[pd.DataFrame, Union[pd.DataFrame, None]]:
+    if set_type not in ["test_set", "train_set"]:
+        raise ValueError(f"'set_type' needs to be one of ['test_set', 'train_set'], got {set_type}")
+
+    caption = '--fixed-test-set' if set_type == "test_set" else "--fixed-train-set"
+
+    fixed_df = None
+    if fixed_set:
+        # fixme Need to add a way of referencing all datasets uniformly i.e. Global ID.
+        fixed_df = df[df["image_name"].isin(fixed_set)]
+        if fixed_df.shape[0] != len(fixed_set):
+            logger.warning(f"The number of instances prescribed in the '{caption}' was not found in the datasets passed")
+            logger.warning(f"'{len(fixed_set)}' instances were requested but only '{fixed_df.shape[0]}' were found.")
+        df = df[~df["image_name"].isin(fixed_set)]
+
+    return df, fixed_df
